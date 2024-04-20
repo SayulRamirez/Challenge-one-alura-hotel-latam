81a249d739b1cb22ea464f056e5192ed8b158003
Sayul <sayulramirez@gmail.com>
2024-04-20 15:07:21 -0600

    Agregado el método countries para cargar los combo box con los paises disponibles

--- src/main/java/com/alura/hotel/utils/Load.java
+++ src/main/java/com/alura/hotel/utils/Load.java
@@ -1,7 +1,8 @@
 package com.alura.hotel.utils;
 
+import com.alura.hotel.enums.CountryEnum;
 import com.alura.hotel.view.MainMenu;
-import com.alura.hotel.view.RegistroHuesped;
+import com.alura.hotel.view.RegisterGuest;
 import com.toedter.calendar.JDateChooser;
 
 import javax.swing.*;
@@ -21,10 +22,16 @@ public class Load {
 
     public static void image(JDateChooser field, String location) {
         try {
-            field.getCalendarButton().setIcon(new ImageIcon(Objects.requireNonNull(RegistroHuesped.class.getResource(location))));
+            field.getCalendarButton().setIcon(new ImageIcon(Objects.requireNonNull(RegisterGuest.class.getResource(location))));
         } catch (NullPointerException e) {
             JOptionPane.showMessageDialog(null,"No se pudo encontrar la imagen en la ruta: " + location);
             throw new RuntimeException(e.getMessage());
+        }
+    }
+
+    public static void countries(JComboBox<String> combo) {
+        for (CountryEnum country: CountryEnum.values()) {
+            combo.addItem(country.getNacion());
         }
     }
 }
